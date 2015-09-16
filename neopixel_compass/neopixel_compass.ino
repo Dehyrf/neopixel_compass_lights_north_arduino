618b36262d70e3a21c55bd46b4947e19d207e3fa
Dehyrf <Dehyrf@users.noreply.github.com>
2015-09-15 23:39:45 -0500

    On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   neopixel_compass/neopixel_compass.ino
    
    Finally got it working without fast blink and touched up code to be more efficient and easer to reverse-engineer.

--- neopixel_compass/neopixel_compass.ino
+++ neopixel_compass/neopixel_compass.ino
@@ -32,7 +32,7 @@ void setup(){
 #endif
  */ // End of trinket special code
   
-  // Serial.begin(9600);
+ // Serial.begin(9600);
   // Serial.print("Setting up I2C ........\n");
   Wire.begin();
   pixels.begin(); 
@@ -50,7 +50,8 @@ void setup(){
  // pixels.setPixelColor(FIRSTPIX, pixels.Color(0,150,0)); // Moderately bright green color.
  // pixels.show(); // This sends the updated pixel color to the hardware.
  
-  theaterChaseRainbow(20);  
+  theaterChaseRainbow(5);  
+  
   digitalWrite(13,HIGH);
 }
 
@@ -75,90 +76,73 @@ void loop(){
   
 */
   compass_heading();
- /* Serial.print ("bearing angle = ");
-  Serial.print ((int)bearing);
-  Serial.println(" Degrees");
-*/
-//lightWay(0);
+ // Serial.print ("bearing angle = ");
+ // Serial.print ((int)bearing);
+ // Serial.println(" Degrees");
+
 
 if((bearing >= 0 && bearing <= 11.25) || (bearing > 348.75 && bearing <= 360))
 {
-  wipe();
   lightWay(0);
 }
 if(bearing > 11.25 && bearing <= 33.75)
 {
-  wipe();
   lightWay(1);
 }
 if(bearing > 33.75 && bearing <= 56.25)
 {
-  wipe();
   lightWay(2);
 }
 if(bearing > 56.25 && bearing <= 78.75)
 {
-  wipe();
   lightWay(3);
 }
 if(bearing > 78.75 && bearing <= 101.25)
 {
-  wipe();
   lightWay(4);
 }
 if(bearing > 101.25 && bearing <= 123.75)
 {
-  wipe();
   lightWay(5);
 }
 if(bearing > 123.75 && bearing <= 146.25)
 {
-  wipe();
   lightWay(6);
 }
 if(bearing > 146.25 && bearing <= 168.75)
 {
-  wipe();
   lightWay(7);
 }
 if(bearing > 168.75 && bearing <= 191.25)
 {
-  wipe();
   lightWay(8);
 }
 if(bearing > 191.25 && bearing <= 213.75)
 {
-  wipe();
   lightWay(9);
 }
 if(bearing > 213.75 && bearing <= 236.25)
 {
-  wipe();
   lightWay(10);
 }
 if(bearing > 236.25 && bearing <= 258.75)
 {
-  wipe();
   lightWay(11);
 }
 if(bearing > 258.75 && bearing <= 281.25)
 {
-  wipe();
   lightWay(12);
 }
 if(bearing > 281.25 && bearing <= 303.75)
 {
-  wipe();
   lightWay(13);
 }
 if(bearing > 303.75 && bearing <= 326.25)
 {
-  wipe();
   lightWay(14);
 }
 if(bearing > 326.25 && bearing <= 348.75)
 { 
-  wipe();
   lightWay(15);
 }
 
@@ -169,38 +153,38 @@ if(bearing > 326.25 && bearing <= 348.75)
   Serial.print(load);
   Serial.println("%");
 */
+
   
-  delay(66 * 2);
+  delay(66); //delay to keep with the clock speed of the magnometer
 }
 
 void lightWay(int offset) {
-  int n = FIRSTPIX + offset;
+  int shift = FIRSTPIX + offset;
+  int n = shift;
     while (n > 15) {n = n - 16;}
-  int w = FIRSTPIX + 4 + offset;
+  int w = shift + 4;
     while (w > 15) {w = w - 16;}
-  int s = FIRSTPIX + 8 + offset;
+  int s = shift + 8;
     while (s > 15) {s = s - 16;}
-  int e = FIRSTPIX + 12 + offset;
+  int e = shift + 12;
     while (e > 15) {e = e - 16;}
- pixels.setPixelColor(n, pixels.Color(255,0,0)); // Moderately bright green color.
- pixels.setPixelColor(s, pixels.Color(0,0,255)); // Moderately bright green color.  
- pixels.setPixelColor(e, pixels.Color(255,255,255)); // Moderately bright green color. 
- pixels.setPixelColor(w, pixels.Color(255,255,255)); // Moderately bright green color.  
+
+ for (int i = 0; i < NUMPIXELS; i++)
+ {
+  pixels.setPixelColor(i, pixels.Color(0,0,0)); // Black/off
+ }
+    
+ pixels.setPixelColor(n, pixels.Color(255,0,0)); // Bright red color.
+ pixels.setPixelColor(s, pixels.Color(0,0,255)); // Moderately bright blue color.  
+ pixels.setPixelColor(e, pixels.Color(255,255,255)); // Bright white color. 
+ pixels.setPixelColor(w, pixels.Color(255,255,255)); // Bright white.  
+ 
  pixels.show(); // This sends the updated pixel color to the hardware.
 }
 
-void wipe() {
-colorWipe(pixels.Color(0, 0, 0), 50);    // Black/off
-}
 
-// Fill the dots one after the other with a color
-void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<pixels.numPixels(); i++) {
-    pixels.setPixelColor(i, c);
-    pixels.show();
-  }
-}
 
+//Below are the classes for the beginning light show and do not need to be changed (probably)
 void theaterChaseRainbow(uint8_t wait) {
   for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
     for (int q=0; q < 3; q++) {
